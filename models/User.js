827253a56fd0bd86c587fac6ac4d8e99c04350f2
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-11-27 13:03:06 -0800

    Create the user.js models

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+	{
+		userName: {
+			type: String,
+			required: true,
+			unique: true,
+			trimmed: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			validate: { // check if the email will have number from 0-9 or letters A-Z followed by @ followed by the domain with dot + the 3 to 7 character for the end of the email, like .com
+				emailCheck: function (email) {
+					let regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+					regex.test(String(email).toLocaleLowerCase().trim());
+				},
+				message: (email) => `${email.value} Please enter a valid email`,
+			},
+		},
+		thoughts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Thought',
+			},
+		],
+		friends: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'User',
+			},
+		],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+	}
+);
+
+userSchema.virtual('friendCount').get(function () {
+	return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
